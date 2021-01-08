759231a648e4d05bd0c1b7e9fe2c396135ed9284
Mohsen Jafari <mohhsen67@gmail.com>
2021-01-08 11:27:41 +0330

    Plate is almost done and just highlight is left.

--- app/components/plate-selector/plate-selector.component.ts
+++ app/components/plate-selector/plate-selector.component.ts
@@ -5,13 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './plate-selector.component.html',
   styleUrls: ['./plate-selector.component.css']
 })
-export class PlateSelectorComponent implements OnInit {
+export class PlateSelectorComponent {
   wells: number = 96;
-  value: string = '';
+  columns: number = 12;
+  letters: Array<string> = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
 
-  constructor() { }
+  // I added some more wells because of the indicators (first row numbers and first column letters)
+  numbers: Array<number> = Array(this.wells + this.columns + (this.wells / this.columns + 1)).fill().map((item, index) => ++index);
 
-  ngOnInit(): void {
+  isFirstRowOrFirstColumn(number: number): boolean {
+    let result: boolean =
+      (this.isFirstColumn(number) ||
+       this.isFirstRow(number)) &&
+       !this.isExactlyFirstCell(number);
+
+    return result;
   }
 
+  isLastColumn(number: number): boolean {
+    return number % (this.columns + 1) == 1;
+  }
+
+  isFirstColumn(number: number): boolean {
+    return number % (this.columns + 1) == 1;
+  }
+
+  isFirstRow(number: number): boolean {
+    return number <= this.columns + 1;
+  }
+
+  isExactlyFirstCell(number: number): boolean {
+    return number == 1;
+  }
 }
