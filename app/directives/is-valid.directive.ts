73da14d1fe8ce1f4a4f783a265d2b09cdab9672e
Mohsen Jafari <mohhsen67@gmail.com>
2021-01-14 00:55:41 +0330

    Cleaning the is-valid directive using ESLint.

--- app/directives/is-valid.directive.ts
+++ app/directives/is-valid.directive.ts
@@ -2,10 +2,11 @@ import { Directive, Input } from '@angular/core';
 import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn } from '@angular/forms';
 const { isNumbersFieldValid } = require('../utils/utils.js');
 
-export function isValidValidator(): ValidatorFn {
-  return (control: AbstractControl): { [key: string]: any } | null =>
+// export function isValidValidator(): ValidatorFn {
+const isValidValidator = (): ValidatorFn =>
+  (control: AbstractControl): { [key: string]: any } | null =>
     isNumbersFieldValid(control.value) ? { isValid: { value: control.value } } : null;
-}
+// }
 
 @Directive({
   selector: '[appIsValid]',
